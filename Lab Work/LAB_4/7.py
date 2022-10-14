4d38ec72e052e2a07355b0b2a80a746147cee994
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-10-14 13:00:19 +0530

    change in lab4q7

--- Lab Work/LAB_4/7.py
+++ Lab Work/LAB_4/7.py
@@ -1,13 +1,15 @@
+import collections
 # anagrams
 test1= ["earth", "heart","tearh"]
 test2=["earth", "hear", "ear"]
 
 def checkAnagram(test):
     flag = True
-    test_result = set(list(test[0]))
+    test_result = list(test[0])
     for item in test:
-        sett = set(list(item))
-        if sett != test_result:
+        sett = list(item)
+        if collections.Counter(sett) != collections.Counter(test_result):
+
             return False
     return flag
 
